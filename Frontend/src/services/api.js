fb6a8b87b1c18775a127771c47e7137968a3cc6d
Ankit Chauhan <44ankitchauhan@gmail.com>
2026-01-01 18:17:19 +0530

    Url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000"
+const baseUrl = "https://movie-recommendation-mukl.onrender.com/"
 
 export const getMovieRecommendations = async (preference) => {
   const response = await fetch(`${baseUrl}/movies/recommend`, {
